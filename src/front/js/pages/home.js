58671150626af28012621984222d74d0d72ddd9a
Mariana <101410421+Placito@users.noreply.github.com>
2023-11-16 20:24:07 +0000

    home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
 import "../../styles/home.css";
 
 export const Home = () => {
@@ -9,28 +10,33 @@ export const Home = () => {
 		<div className="container mt-5">
 			<h1>Choose your role:</h1>
 			<div className="roles-box mt-5">
-				<div className="card" style={{ width: "60rem" }}>
-					<img src="..." className="card-img-top" alt="..." />
-					<p>Director</p>
-				</div>
-				<div className="card" style={{ width: "60rem" }}>
-					<img src="..." className="card-img-top" alt="..." />
-					<p>Administrative</p>
-				</div>
-				<div className="card" style={{ width: "60rem" }}>
-					<img src="..." className="card-img-top" alt="..." />
-					<p>Techinician</p>
-				</div>
+				<Link  to="/login">
+					<div className="card card-1" style={{ width: "20rem" }}>
+						<p>Director</p>
+					</div>
+				</Link>	
+				<Link  to="/login">
+					<div className="card card-2" style={{ width: "20rem" }}>
+						<p>Administrative</p>
+					</div>
+				</Link>
+				<Link  to="/login">
+					<div className="card card-3" style={{ width: "20rem" }}>
+						<p>Techinician</p>
+					</div>
+				</Link>	
 			</div>
 			<div className="roles-box mt-5">
-				<div className="card" style={{ width: "60rem" }}>
-					<img src="..." className="card-img-top" alt="..." />
-					<p>Operator</p>
-				</div>
-				<div className="card" style={{ width: "60rem" }}>
-					<img src="..." className="card-img-top" alt="..." />
-					<p>Quality Controler</p>
-				</div>
+				<Link  to="/login">
+					<div className="card card-4" style={{ width: "20rem" }}>
+						<p>Operator</p>
+					</div>
+				</Link>	
+					<Link  to="/login">
+					<div className="card card-5" style={{ width: "20rem" }}>
+						<p>Quality Controler</p>
+					</div>
+				</Link>	
 			</div>
 		</div>
 	);
