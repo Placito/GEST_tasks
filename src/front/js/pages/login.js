ea4b2e70552e964d8cc16b1957378b940099a040
Mariana <101410421+Placito@users.noreply.github.com>
2023-11-20 20:51:07 +0000

    login page

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import PropTypes from "prop-types";
+import "../../styles/login.css";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
@@ -8,7 +8,71 @@ export const Login = () => {
 	const params = useParams();
 
 	return (
-		<div className="jumbotron">
-		</div>
+		<div className="container">
+			<h4 className="text-login">Login to your account:</h4>
+			<div >
+			  <br />
+			  <div className="form-outline mb-4">
+				<input
+				  type="email"
+				  id="typeEmailX-2"
+				  className="form-control-lg form-input-login"
+				  placeholder="Username"
+				  required
+				/>
+			  </div>
+			  <div className="form-outline mb-4">
+				<input
+				  type="password"
+				  id="typePasswordX"
+				  className="form-control-lg form-input-login"
+				  placeholder="Password"
+				  required
+				/>
+			  </div>
+			  <div className="form-check d-flex row mt-4 p-0 ms-0">
+				<div className="d-flex ">
+				  <div className=" d-flex col-6 ">
+					<input
+					  className="form-check-input-login me-2"
+					  type="checkbox"
+					  value=""
+					  id="form1Example3"
+					/>
+					  <label className="form-check-label remember" htmlFor="form1Example3">
+						{" "}
+						Remember password{" "}
+					  </label>
+				</div>
+				<Link
+				  className="forgot-link link col-6"
+				  to="/resetPassword"
+				  type="submit"
+				>
+				  Forgot Password?
+				</Link>
+			  </div>
+			  </div>
+			  <br/>
+			  <br/>
+			  <br/>
+			  <br/>
+			  <div className="d-flex justify-content-between">
+				<button
+				  className="btn-login"
+				>
+				  Login
+				</button>
+				<Link className="link" to="/signup">
+				  <button
+					className="btn-login"
+					type="submit"
+				  >
+					Register
+				  </button>
+				</Link>
+			  </div>
+			</div>
+		  </div>
 	);
 };
