bd12288470da9a17b8ff24b62c6a0d40a144481e
Mariana <101410421+Placito@users.noreply.github.com>
2023-11-21 19:45:06 +0000

    chooses

--- src/front/js/pages/choose_2.js
+++ src/front/js/pages/choose_2.js
@@ -1,13 +1,25 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import "../../styles/home.css";
+import "../../styles/choose.css";
 
 export const Choose_2 = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
+		<div className="container mt-5">
+			<div className="box mt-5">
+				<div className="title">
+					<h1>These are the function you can perform:</h1>
+				</div>
+				<div className="buttons">
+					<button className="btn-tasks" type="submit">Consult Users</button>
+					<button className="btn-tasks" type="submit">Consult Sectors</button>
+				</div>
+			</div>
+			<div className="set-arrow">
+				<div className="legend-arrow" >Go Back</div>
+				<div className="btn-arrow" type="submit"><i className="fa-solid fa-arrow-left"></i></div>
+			</div>
 			
 		</div>
 	);
