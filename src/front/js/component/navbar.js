22ef559b6b1271630f44f68a504a1f22a5defaa6
Mariana <101410421+Placito@users.noreply.github.com>
2023-12-05 16:10:46 +0000

    add link import

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
 import logo from "../../img/logo.png";
 import "../../styles/navbar.css";
 
