fc0254a019c3dd4516cfcb064b85e5752d47fcc0
Mariana <101410421+Placito@users.noreply.github.com>
2024-01-15 17:37:49 +0000

    endpoint resetPassword

--- src/api/routes.py
+++ src/api/routes.py
@@ -119,3 +119,47 @@ def reset_password():
         return jsonify({"msg": "Password reset successful."}), 200
     except Exception as e:
         return jsonify({"msg": "An error occurred", "error": str(e)}), 500
+    
+    # endpoint for send an email with a link to reset password
+@main.route("/resetPassword", methods=["POST"])
+@cross_origin(origin="process.env.FRONTEND_URL")
+def send_reset_email():
+    try:
+        # Ensure that the request has the correct Content-Type header
+        if request.headers["Content-Type"] != "application/json":
+            return (
+                jsonify(
+                    {
+                        "error": "415 Unsupported Media Type: Content-Type must be 'application/json'"
+                    }
+                ),
+                415,
+            )
+
+        email = request.json.get("email")
+        print(email)
+
+        # Query the database to check if the email exists
+        user = User.query.filter_by(email=email).first()
+        print(user)
+
+        if user is None:
+            return jsonify({"msg": "User with this email does not exist."}), 404
+        else:
+            # Generate an access token and construct the reset link
+            token = create_access_token(identity=user.email)
+            link = f"https://silver-cod-gvp74jvvwjqc9vxp-3000.app.github.dev/newPassword?token={token}"
+
+            message = Message(
+                subject="Password Reset Link",
+                sender=current_app.config["MAIL_USERNAME"],
+                recipients=[email],
+                body="Hey, this is a link for resetting the password.",
+                html=f"Reset your password with this link: <a href='{link}'>Reset Password</a>",
+            )
+
+            mail.send(message)
+            return jsonify({"message": "Password reset email sent successfully"}), 200
+    except Exception as e:
+        return jsonify({"message": "An error occurred", "error": str(e)}), 500
+
