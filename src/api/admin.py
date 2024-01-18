651db9b63e6c7a907c71e5b8df4ab412cec9e964
Mariana <101410421+Placito@users.noreply.github.com>
2024-01-18 16:32:29 +0000

    creating custom_admin_styles.css

--- src/api/admin.py
+++ src/api/admin.py
@@ -6,6 +6,8 @@ from flask_admin.contrib.sqla import ModelView
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'darkly'
+    # Include my custom CSS file
+    app.config['FLASK_ADMIN_CSS'] = 'path/to/custom_admin_styles.css'
     admin = Admin(app, name='GEST Admin', template_mode='bootstrap3')
     
     # Add User model to the admin
