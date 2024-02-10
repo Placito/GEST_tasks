a70c64b11ce5af0d41cf2eaa8378a27df1843a4b
Mariana <101410421+Placito@users.noreply.github.com>
2024-02-10 12:23:04 +0000

    new migration in users

--- src/api/models.py
+++ src/api/models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True, nullable=False)
-    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
     role = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
 
