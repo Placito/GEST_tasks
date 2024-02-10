bfe2bd4af5062a8ccc1892e94d995b7baa19b0d5
Mariana <101410421+Placito@users.noreply.github.com>
2024-02-10 12:21:12 +0000

    add value to the form in addUsers

--- src/front/js/component/addUser.js
+++ src/front/js/component/addUser.js
@@ -1,32 +1,54 @@
 import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
+import axios from 'axios';
 import "../../styles/table.css";
 
 export const AddUser = () => {
     const { store, actions } = useContext(Context);
-
-    //function to change the value from on an specific id of an input an assing it to a new object
-    function onChangeForm(e) {
-        const { id, value } = e.target;
-        console.log({id, value});
-    }
+    const [formData, setFormData] = useState({
+            username: '',
+            email: '',
+            password: '',
+            role: ''
+        });
+    
+        const handleChange = (e) => {
+            setFormData({ ...formData, [e.target.name]: e.target.value });
+        };
+    
+        const handleSubmit = async (e) => {
+            e.preventDefault();
+            try {
+                const response = await axios.post('/create-user', formData);
+                console.log(response.data.message);
+                // Reset form after successful submission
+                setFormData({
+                    username: '',
+                    email: '',
+                    password: '',
+                    role: ''
+                });
+            } catch (error) {
+                console.error('Error creating user:', error.response.data.error);
+            }
+        };
 
 	return (
 		<div className="container">
             <h1>Add a new User:</h1>
-			<form onChange={(e) =>  onChangeForm(e)} onSubmit={(e) => e.preventDefault()}>
+			<form onChange={(e) => handleChange(e)} onSubmit={handleSubmit}>
                 <div className="mb-3 row">
-                    <input type="text" className="form-control" id="username" placeholder="Enter username" />
+                    <input type="text" value={formData.username} className="form-control" name="username" placeholder="Enter username" />
                 </div>
                 <div className="mb-3 row">
-                    <input type="name" className="form-control" id="name" placeholder="Enter name" />
+                    <input type="email" value={formData.email} className="form-control" name="email" placeholder="Enter email" />
                 </div>
                 <div className="mb-3 row">
-                    <input type="text"  className="form-control" id="role" placeholder="Enter role" />
+                    <input type="text" value={formData.role} className="form-control" name="role" placeholder="Enter role" />
                 </div>
                 <div className="mb-3 row">
-                    <input type="text" className="form-control" id="password" placeholder="Enter password" />
+                    <input type="password" value={formData.password} className="form-control" name="password" placeholder="Enter password" />
                 </div>
                 <Link className="link-user" to="/table_users">
                     <div className="row"> 
