5569ae22e8b67810805c4139876b11eea43e608c
Mariana <101410421+Placito@users.noreply.github.com>
2024-08-09 09:49:26 +0000

    route for the role director

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,8 +1,10 @@
+from os import abort
+import sys
 from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
 from flask_jwt_extended import create_access_token, unset_jwt_cookies, jwt_required
-from api.models import db, User
+from api.models import Seccion_1, Seccion_2, Seccion_3, Seccion_4, Seccion_5, Seccion_6, db, User
 from flask_cors import CORS, cross_origin
 
 api = Blueprint('api', __name__)
@@ -17,7 +19,7 @@ ROLES = {
     'quality': ['quality'],
 }
 
-# Login route
+# Login route 
 @api.route('/login', methods=['POST'])
 @cross_origin()
 def login_post():
@@ -62,3 +64,37 @@ def create_user():
 
     access_token = create_access_token(identity=new_user.id)
     return jsonify({'success': 'true', "access_token": access_token}), 201
+
+# route for director
+@api.route('/director', methods=['GET'])
+@login_required.role('director')
+def director():
+    error = False
+    body = {}
+    try:
+        # Fetch all sections
+        seccion_1 = [sec.serialize() for sec in Seccion_1.query.all()]
+        seccion_2 = [sec.serialize() for sec in Seccion_2.query.all()]
+        seccion_3 = [sec.serialize() for sec in Seccion_3.query.all()]
+        seccion_4 = [sec.serialize() for sec in Seccion_4.query.all()]
+        seccion_5 = [sec.serialize() for sec in Seccion_5.query.all()]
+        seccion_6 = [sec.serialize() for sec in Seccion_6.query.all()]
+
+        # Combine all sections into the response body
+        body['seccion_1'] = seccion_1
+        body['seccion_2'] = seccion_2
+        body['seccion_3'] = seccion_3
+        body['seccion_4'] = seccion_4
+        body['seccion_5'] = seccion_5
+        body['seccion_6'] = seccion_6
+
+    except Exception as e:
+        db.session.rollback()
+        error = True
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(500)
+    else:
+        return jsonify(body)
