e84b1c12838aa0b6e18800b3acfd73142ec8de1b
Mariana <101410421+Placito@users.noreply.github.com>
2024-08-29 12:56:59 +0000

    add if statements of the roles in login route

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, redirect, url_for
 from werkzeug.security import check_password_hash
 from flask_jwt_extended import create_access_token
 from flask_cors import CORS, cross_origin
@@ -16,29 +16,38 @@ ROLES = {
     'quality': ['quality'],
 }
 
-# Login route 
+# Login route
 @api.route('/login', methods=['POST'])
 @cross_origin()
 def login_post():
     # Retrieve username and password from the request
     username = request.json.get('username', None)
     password = request.json.get('password', None)
-    
+
     # Query the user by username
     user = User.query.filter_by(username=username).first()
-    
+
     if user and check_password_hash(user.password, password):
         role = user.role
-        
+
         # Validate the role
         if role not in ROLES:
             return jsonify({"success": "false", "msg": "Invalid role assigned to the user"}), 400
-        
+
+        # Redirect based on role
+        if role == 'director':
+            return redirect(url_for('choose_1'))
+        elif role == 'administrative':
+            return redirect(url_for('choose_2'))
+        elif role in ['technician', 'operator', 'quality']:
+            return redirect(url_for('choose_3'))
+
         # Create access token with role information
         access_token = create_access_token(identity=user.id, additional_claims={"role": role})
-        
+
         # Respond with success, token, and role
         return jsonify({"success": "true", "access_token": access_token, "role": role}), 200
+
     else:
         # Respond with error if username or password is incorrect
         return jsonify({"success": "false", "msg": "Bad username or password"}), 401
