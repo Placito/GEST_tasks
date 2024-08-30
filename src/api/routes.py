347279271b64fd7a6066453a50bdc34f5cdcfeb9
Mariana <101410421+Placito@users.noreply.github.com>
2024-08-30 16:23:15 +0000

    add rout for reset and new pass

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,3 +1,5 @@
+from asyncio import current_task
+from mailbox import Message
 from flask import Blueprint, request, jsonify, redirect, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token
@@ -52,7 +54,73 @@ def login_post():
         # Respond with error if username or password is incorrect
         return jsonify({"success": "false", "msg": "Bad username or password"}), 401
 
+# endpoint for send an email with a link to reset password
+@api.route("/resetPassword", methods=["POST"])
+@cross_origin(origin="process.env.FRONTEND_URL")
+def send_reset_email():
+    try:
+        # Ensure that the request has the correct Content-Type header
+        if request.headers["Content-Type"] != "application/json":
+            return (
+                jsonify(
+                    {
+                        "error": "415 Unsupported Media Type: Content-Type must be 'application/json'"
+                    }
+                ),
+                415,
+            )
 
+        email = request.json.get("email")
+        print(email)
+
+        # Query the database to check if the email exists
+        user = User.query.filter_by(email=email).first()
+        print(user)
+
+        if user is None:
+            return jsonify({"msg": "User with this email does not exist."}), 404
+        else:
+            # Generate an access token and construct the reset link
+            token = create_access_token(identity=user.email)
+            link = f"https://silver-cod-gvp74jvvwjqc9vxp-3000.app.github.dev/newPassword?token={token}"
+
+            message = Message(
+                subject="Password Reset Link",
+                sender=current_task.config["MAIL_USERNAME"],
+                recipients=[email],
+                body="Hey, this is a link for resetting the password.",
+                html=f"Reset your password with this link: <a href='{link}'>Reset Password</a>",
+            )
+
+            mail.send(message)
+            return jsonify({"message": "Password reset email sent successfully"}), 200
+    except Exception as e:
+        return jsonify({"message": "An error occurred", "error": str(e)}), 500
+
+
+# Endpoint for updating the password
+@api.route("/newPassword", methods=["POST"])
+@jwt_required()
+@cross_origin()
+def reset_password():
+    try:
+        password = request.json.get("password", None)
+        email = get_jwt_identity()
+
+        # Query the database to check if the email exists
+        user = User.query.filter_by(email=email).first()
+        if user is None:
+            return jsonify({"msg": "User with this email does not exist."}), 404
+        # Update the user's password
+        User.password = password
+        print(User.password)
+        # Commit the changes to the database
+        db.session.commit()
+
+        return jsonify({"msg": "Password reset successful."}), 200
+    except Exception as e:
+        return jsonify({"msg": "An error occurred", "error": str(e)}), 500
+    
 # Routes for sectors
 @api.route('/sectors', methods=['GET'])
 def get_sectors():
