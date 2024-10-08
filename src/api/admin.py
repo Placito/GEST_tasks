0504d7147b1f8c840bf6ae7909ba522ecee7197c
Mariana <101410421+Placito@users.noreply.github.com>
2024-10-08 14:28:59 +0000

    make a base.html to style the dashboard

--- src/api/admin.py
+++ src/api/admin.py
@@ -7,14 +7,13 @@ from .models import db, User, Seccion_1, Seccion_2, Seccion_3, Seccion_4, Seccio
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'darkly'
-    # Include my custom CSS file
-    app.config['FLASK_ADMIN_CSS'] = 'front/styles/custom_admin_styles.css'  
-    
-    admin = Admin(app, name='GEST Admin', template_mode='bootstrap3')
+    app.config['FLASK_ADMIN_CSS'] = 'front/styles/custom_admin_styles.css'  # Make sure this path is correct
+
+    admin = Admin(app, name='GEST Admin', base_template='admin/base.html')  # Ensure this points to your custom base template
 
     # Add User model to the admin with an icon
     admin.add_view(ModelView(User, db.session))
-    # Add Seccion models to the admin with icons and change the display name to "sectors"
+    # Add Seccion models to the admin with icons
     admin.add_view(ModelView(Seccion_1, db.session, category='Sectors', name='Sector 1', menu_icon_type='glyph', menu_icon_value='glyphicon-list-alt'))
     admin.add_view(ModelView(Seccion_2, db.session, category='Sectors', name='Sector 2', menu_icon_type='glyph', menu_icon_value='glyphicon-list-alt'))
     admin.add_view(ModelView(Seccion_3, db.session, category='Sectors', name='Sector 3', menu_icon_type='glyph', menu_icon_value='glyphicon-list-alt'))
